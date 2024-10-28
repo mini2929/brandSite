599bdf40ceccee6c3ee475a9c5c9c01a0d09ba9a
mini2929 <haloyun29@gmail.com>
2024-10-28 20:25:36 +0900

    반응형 전 완성

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -29,31 +29,52 @@ export default function BrandStory() {
 				</div>
 			</article>
 
-			<section className='mid'>
-				<div className='mid1'></div>
-				<div className='mid2'></div>
+			<section className='mid_1'>
+				<div className='mid_1-1'>
+					<div className='minibox'>
+						<p>
+							All day /<br /> All together /<br /> All in One
+						</p>
+						<p>멋진 남성으로 기억되는 그 시작</p>
+					</div>
+				</div>
+				<div className='mid_1-2'>
+					<p>
+						자연유래성분으로 피부에 자극없이 부드럽게 감싸며 하루의 시작과 끝을 함께하는 All day/ All together/ All in
+						One 지향
+					</p>
+					<p>완벽한 서포트를 꿈꾸는 아발론</p>
+				</div>
 			</section>
 
-			<section>
-				{memberData.map((data, idx) => {
-					if (idx !== 0) {
-						return (
-							// 리턴문 안쪽에 제일 부모 jsx요소엔 무조건 key={순서값} 적용
-							<article key={idx}>
-								<div className='pic'>
-									<img className='others' src={data.pic} alt={data.name} />
-								</div>
-								<h3>{data.name}</h3>
-								<p>{data.text}</p>
-							</article>
-						);
-					}
-				})}
+			<section className='mid_2'>
+				{memberData.slice(1, 3).map((data, idx) => (
+					<article key={idx + 1}>
+						<div className='pic'>
+							<img className='others' src={data.pic} alt={data.name} />
+						</div>
+						<h3>{data.name}</h3>
+						<p>{data.text}</p>
+					</article>
+				))}
 			</section>
 
 			<section className='last'>
-				<div className='last1'></div>
-				<div className='last2'></div>
+				<img className='perfume' src={memberData[3].pic} alt={memberData[3].name} />
+
+				<div className='lastText'>
+					<p>완벽함의 마침표</p>
+					<p>
+						Orgainc Based
+						<br /> Classified Perfume
+					</p>
+					<div
+						className='buttons
+					'>
+						<button>information</button>
+						<button>pictures</button>
+					</div>
+				</div>
 			</section>
 		</Layout>
 	);
